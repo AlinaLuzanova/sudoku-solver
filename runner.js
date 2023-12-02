7440598eb5c515167b8c2912338ae789f71d372c
test <alinaluzanova@gmail.com>
2023-12-02 14:25:48 +0300

    Add DOM

--- runner.js
+++ runner.js
@@ -1,8 +1,7 @@
 // Подключить модуль работы с файловой системой.
-const fs = require('fs');
-
 // Подключить функцию readAndSolve из соответствующего файла.
-const readAndSolve = require('./readAndSolve');
+import fs from './fs'
+import readAndSolve from './readAndSolve.js';
 
 // Все судоку для решения доступны в файле puzzles.txt.
 // Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
--- sudoku.js
+++ sudoku.js
@@ -3,19 +3,24 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
+export function showSudoku(boardString) {
   const filledSudokuArr = [];
+  const rows = document.querySelectorAll('.row');
   const sudokuString = boardString.split('');
   for (let i = 0; i < 9; i++) {
+    const rowChildren = rows[i].querySelectorAll('.item');
     const rowArr = [];
     filledSudokuArr.push(rowArr);
     for (let j = 0; j < 9; j++) {
       const index = i * 9 + j; // вычисляем индекс в sudokuString
       if (index < sudokuString.length) {
         rowArr.push(sudokuString[index]);
+        rowChildren[j].innerText = sudokuString[index];
       }
     }
   }
+
+
   return filledSudokuArr; //массив заполненный строчкой из puzzle.txt
 }
 
@@ -23,7 +28,7 @@ function solve(boardString) {
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
+export function isSolved(board) {
 
 }
 
@@ -32,13 +37,9 @@ function isSolved(board) {
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
+export function prettyBoard(board) {
 
 }
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
-module.exports = {
-  solve,
-  isSolved,
-  prettyBoard,
-};
+
