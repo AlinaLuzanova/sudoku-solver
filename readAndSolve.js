3f9a7f8582d57f98974fdb640c72a1f9c3438506
SerovNM <5991nike@gmail.com>
2023-12-02 17:23:30 +0300

    deleted runner.js

--- readAndSolve.js
+++ readAndSolve.js
@@ -1,19 +1,27 @@
 // Подключить функции из файла sudoku.js.
-import { showSudoku, prettyBoard } from "./sudoku.js";
-import { solveSudoku } from "./solved.js";
+import { showSudoku, prettyBoard } from './sudoku.js';
+import { solveSudoku } from './solved.js';
 
-const input = document.querySelector(".input");
-const btnShow = document.querySelector(".btnShow");
-const btnSolve = document.querySelector(".btnSolve");
+document.getElementById('inputfile').addEventListener('change', function () {
+  let fr = new FileReader();
+  fr.onload = function () {
+    document.getElementById('output').textContent = fr.result;
+  };
+  fr.readAsText(this.files[0]);
+});
+
+const input = document.querySelector('.input');
+const btnShow = document.querySelector('.btnShow');
+const btnSolve = document.querySelector('.btnSolve');
 
 function readAndSolve() {
   // Разбить содержимое файла построчно и отфильтровать все пустые строки.
-  const puzzles = document.getElementById("output").innerText;
-  const rows = puzzles.split("\n").filter((line) => line !== "");
+  const puzzles = document.getElementById('output').innerText;
+  const rows = puzzles.split('\n').filter(line => line !== '');
 
   // Получить номер судоку из process.argv, либо взять 1-й судоку по умолчанию.
   let puzzleNumber = input.value;
-  input.value = "";
+  input.value = '';
 
   // Ограничить номер судоку максимальным числом массива с паззлами.
   if (puzzleNumber > rows.length) {
@@ -26,7 +34,7 @@ function readAndSolve() {
   showSudoku(puzzle);
 
   // Использовать функцию solve из файла sudoku.js для решения судоку.
-  btnSolve.addEventListener("click", () => {
+  btnSolve.addEventListener('click', () => {
     solveSudoku(showSudoku(puzzle));
     if (solveSudoku(showSudoku(puzzle))) {
       // alert(`Не смогли решить судоку №${puzzleNumber} :(, '\n')`);
@@ -41,6 +49,6 @@ function readAndSolve() {
   // игрового поля в строку в желаемом формате.
 }
 
-btnShow.addEventListener("click", readAndSolve);
+btnShow.addEventListener('click', readAndSolve);
 
 export default readAndSolve;
