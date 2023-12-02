b5e4f36c16edd65be45b5ef9d8a8837a73ceb9db
test <alinaluzanova@gmail.com>
2023-12-02 15:31:56 +0300

    add connection

--- solved.js
+++ solved.js
@@ -1,80 +1,95 @@
-const showSudoku = require("./sudoku");
+//const showSudoku = require("./sudoku");
+import { showSudoku } from './sudoku';
+// eslint-disable-next-line import/prefer-default-export
+export function solveSudoku(board) {
+  function innerFunc() {
+    const size = 9;
+    const boxSize = 3;
 
-function solveSudoku(board) {
-  const size = 9;
-  const boxSize = 3;
-
-  const findEmpty = (board) => {
-    for (let r = 0; r < size; r++) {
-      for (let c = 0; c < size; c++) {
-        if (board[r][c] === "-") {
-          return [r, c];
+    const findEmpty = (board) => {
+      for (let r = 0; r < size; r++) {
+        for (let c = 0; c < size; c++) {
+          if (board[r][c] === "-") {
+            return [r, c];
+          }
         }
       }
-    }
-    return null;
-  };
+      return null;
+    };
 
-  const validate = (num, pos, board) => {
-    const [r, c] = pos;
+    const validate = (num, pos, board) => {
+      const [r, c] = pos;
 
-    //Check rows
-    for (let i = 0; i < size; i++) {
-      if (board[i][c] === num && i !== r) {
-        return false;
-      }
-    }
-
-    //Check cols
-    for (let i = 0; i < size; i++) {
-      if (board[r][i] === num && i !== c) {
-        return false;
-      }
-    }
-
-    //Check box
-    const boxRow = Math.floor(r / boxSize) * boxSize;
-    const boxCol = Math.floor(c / boxSize) * boxSize;
-
-    for (let i = boxRow; i < boxRow + boxSize; i++) {
-      for (let j = boxCol; j < boxCol + boxSize; j++) {
-        if (board[i][j] === num && i !== r && j !== c) {
+      //Check rows
+      for (let i = 0; i < size; i++) {
+        if (board[i][c] === num && i !== r) {
           return false;
         }
       }
-    }
 
-    return true;
-  };
-
-  const solve = () => {
-    const currPos = findEmpty(board);
-
-    if (currPos === null) {
-      return true;
-    }
-    //console.log('------------------------------');
-    for (let i = 1; i < size + 1; i++) {
-      const currNum = i.toString();
-      const isValid = validate(currNum, currPos, board);
-      //console.log('currPos ', currPos, 'currNum ',currNum, 'isValid ',isValid);
-      if (isValid) {
-        const [x, y] = currPos;
-        board[x][y] = currNum;
-
-        if (solve()) {
-          return true;
+      //Check cols
+      for (let i = 0; i < size; i++) {
+        if (board[r][i] === num && i !== c) {
+          return false;
         }
+      }
 
-        board[x][y] = ".";
+      //Check box
+      const boxRow = Math.floor(r / boxSize) * boxSize;
+      const boxCol = Math.floor(c / boxSize) * boxSize;
+
+      for (let i = boxRow; i < boxRow + boxSize; i++) {
+        for (let j = boxCol; j < boxCol + boxSize; j++) {
+          if (board[i][j] === num && i !== r && j !== c) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    };
+
+    const solve = () => {
+      const currPos = findEmpty(board);
+
+      if (currPos === null) {
+        return true;
+      }
+      //console.log('------------------------------');
+      for (let i = 1; i < size + 1; i++) {
+        const currNum = i.toString();
+        const isValid = validate(currNum, currPos, board);
+        //console.log('currPos ', currPos, 'currNum ',currNum, 'isValid ',isValid);
+        if (isValid) {
+          const [x, y] = currPos;
+          board[x][y] = currNum;
+
+          if (solve()) {
+            return true;
+          }
+
+          board[x][y] = ".";
+        }
+      }
+
+      return false;
+    };
+
+    solve();
+    return board;
+  }
+  function loadToDOM() {
+    const rows = document.querySelectorAll('.row');
+    const sudokuString = String(innerFunc()).split('');
+    for (let i = 0; i < 9; i++) {
+      const rowChildren = rows[i].querySelectorAll('.item');
+      for (let j = 0; j < 9; j++) {
+        const index = i * 9 + j; // вычисляем индекс в sudokuString
+        if (index < sudokuString.length) {
+          rowChildren[j].innerText = sudokuString[index];
+        }
       }
     }
-
-    return false;
-  };
-
-  solve();
-  return board;
+  }
+  return loadToDOM();
 }
-
-console.table(solveSudoku(showSudoku));
